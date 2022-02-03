f94c4b9f3fc1ee669cc6727c4ef73c19b3a812c4
dharma <dharma@telio.vn>
2022-02-03 20:45:24 +0530

    update code

--- hello.py
+++ hello.py
@@ -1,18 +1,21 @@
 from flask import Flask, jsonify, request
+
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello_world():
+  print("Hello World!")
   return "Hello, World!"
 
 incomes = [
-  { 'description': 'salary', 'amount': 5000 }
+  { 'description': 'salary', 'amount': 50000 }
 ]
 
 
 @app.route('/incomes')
 def get_incomes():
+  print("getting incomes",incomes)
   return jsonify(incomes)
 
 
@@ -20,3 +23,6 @@ def get_incomes():
 def add_income():
   incomes.append(request.get_json())
   return '', 204
+
+if __name__ == "__main__":
+    app.run(debug=True)
